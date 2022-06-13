d00c9868118958fcbe990609cc459da555f6ab00
jedromz <romankiewicz.j@gmail.com>
2022-06-13 10:54:56 +0200

    Initial commit

---
+++ src/test/java/com/mas/pjatk/masfinalproject/MasFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mas.pjatk.masfinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
